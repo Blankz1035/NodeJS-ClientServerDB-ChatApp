ef20e88b7190c8068a5f9c7f7f96a672d54a9ab1
Andy Blankley <andrew.blankley@hotmail.com>
2019-09-21 15:10:45 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# NodeJS-ClientServerDB-ChatApp
+
+Applications such as WhatsApp and Viber have always intrigued me on the developments of these type of application architectures. This project was a "toe in the pond" of possibilities of enhancement that i am currently working on for an Application.
+
+Demo chat application with push notifications of new messages sent from 1 client to another. Messages are stored using NoSQL DB MongoDB. 
+
+## Getting Started
+
+To check out this application, go ahead and install the dependencies using the Package.JSON file and NPM install. Run NODEMON  (great module in your projects!) to initialise the server and open Chrome at LocalHost:3001.
+
+### Prerequisites
+
+```
+1. NodeJS.
+2. Chrome Browser.
+3. Nodemon module (would be a plus for live reloading).
+```
+
+## Thanks for review!
+
---
+++ Server.js
@@ -0,0 +1,64 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
+
+//DB Connection using the MongoDB 
+//const dbUrl = Removing connect url for GitHub Upload.
+const dbUrl = "mongodb+srv://<DBUser:<Password>@cluster1test-6bfdl.mongodb.net/<ClusterName>?retryWrites=true&w=majority";
+
+var server = http.listen(3001, () => {
+	console.log('Server is listening on port ', server.address().port)
+})
+
+io.on('connection', (socket) => {
+	console.log('User connected..')
+})
+
+mongoose.connect(dbUrl, { useNewUrlParser: true }, (err) =>{
+    console.log('mongo db connection', err)
+})
+
+mongoose.Promise = Promise
+
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
+
+app.get('/messages', (req,res) => {
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
+}) 
+
+app.post('/messages', async (req, res) => {
+
+    try {
+        var message = new Message(req.body)
+        var savedMessage = await message.save()
+        console.log('saved', savedMessage)
+
+        var censored = await Message.findOne({ message: 'badword' })
+
+        if (censored)
+            await Message.remove({ _id: censored.id })
+        else
+            io.emit('message', req.body)
+
+        res.sendStatus(200)
+    } catch (error) {
+        res.sendStatus(500)
+        return console.error(error)
+    } finally {
+        console.log('message post called')
+    }
+})
+
